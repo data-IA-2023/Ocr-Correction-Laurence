b0390c623c5e5902ad861845e4bc672141f3ecf7
L. Berville <leabizbille@hotmail.com>
2024-04-09 11:13:22 +0200

    Version 11h13

--- controler.py
+++ controler.py
@@ -2,18 +2,15 @@ print("start controler")
 #Note : Appli « MVC » FastAPI 
 
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+#from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
 templates = Jinja2Templates(directory="templates")
-
 
 @app.get("/items/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
