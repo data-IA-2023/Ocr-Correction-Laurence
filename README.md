acdadb42b195af1a411816f5fdeedf842f3a3ffa
L. Berville <leabizbille@hotmail.com>
2024-04-09 10:55:42 +0200

    Version 10h55

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ VISION_KEY : Clé vision
 VISION_ENDPOINT: Clé vision
 DATABASE_URL=sqlite:///ocr.sqlite 
 DISCORD_OCR : Lien vers discord
+
+
+pip install -r requirements.txt
+Env/Scripts/activate
--- controler.py
+++ controler.py
@@ -6,6 +6,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
